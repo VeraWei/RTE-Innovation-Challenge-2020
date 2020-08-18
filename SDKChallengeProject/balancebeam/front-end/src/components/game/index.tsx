41e65af3ca45f9ba4bb5c42e6fa6ada547cbad61
Vera Wei <vera.wqm@gmail.com>
2020-08-18 13:11:23 -0700

    update components

---
+++ SDKChallengeProject/balancebeam/front-end/src/components/game/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+
+class Game extends React.Component {
+
+    render() {
+
+    return (
+        <div className="game-container">
+            This is game container
+        </div>
+    );
+    }
+}
+
+export default Game;
---
+++ SDKChallengeProject/balancebeam/front-end/src/components/video/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+
+class VideoComponent extends React.Component {
+
+    render() {
+
+    return (
+        <div className="video-container">
+            This is video container
+        </div>
+    );
+    }
+}
+
+export default VideoComponent;
